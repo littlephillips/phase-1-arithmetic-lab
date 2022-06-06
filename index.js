484186f8d1626050147cd42c07641382ebe8e5a7
littlephillips <phillipkinuthia72@gmail.com>
2022-06-06 23:25:44 +0300

    lab commit

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+
+
+function multiplyFun(num1, num2) {
+    let myMultiplication = num1 * num2;
+    return myMultiplication( 31, 2)
+
+}
+
+function randomNumber(){
+    let myRandomNumber = Math.floor(Math.random() * 10) + 1;
+    return myRandomNumber;
+}
+
+ function modFun(num3, num4) {
+     let myMod = num3 % num4;
+     return myMod(16, 4)
+ }
+
+function max(){
+    let myMax =   Math.max(2, 9, 20, 10, 18, 1);
+    return myMax;
+}
+
+ 
