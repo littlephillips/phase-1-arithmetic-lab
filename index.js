7414ff19a506490365af0a6fcb5918a72cbfc983
littlephillips <phillipkinuthia72@gmail.com>
2022-06-08 15:11:02 +0300

    edited index.js

--- index.js
+++ index.js
@@ -2,26 +2,19 @@
 // Write your code here
 
 
-function multiplyFun(num1, num2) {
-    let myMultiplication = num1 * num2;
-    return myMultiplication( 31, 2)
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-}
+const random = Math.floor( Math.random() * 10 ) + 1;
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
 
 
-function randomNumber(){
-    let myRandomNumber = Math.floor(Math.random() * 10) + 1;
-    return myRandomNumber;
-}
+const max = Math.max( 5, 10, 15, 20);
 
- function modFun(num3, num4) {
-     let myMod = num3 % num4;
-     return myMod(16, 4)
- }
-
-function max(){
-    let myMax =   Math.max(2, 9, 20, 10, 18, 1);
-    return myMax;
-}
+ 
 
  
